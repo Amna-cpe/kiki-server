49187db2d39a70b1e9d3cde3d19afcca353fdfb6
Amna <ajaldh26x@gmail.com>
2021-01-23 10:38:12 +0300

    modify

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = new ApolloServer({
   resolvers,
   context: ({ req }) => ({ req , pubsub}),
 });
-const PORT = propcess.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
